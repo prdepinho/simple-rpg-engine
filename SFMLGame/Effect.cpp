16ea15f4e4d01be98a496ce38f8ec6737aed6fe2
Paulo Pinho <prdepinho@gmail.com>
2019-08-17 12:07:00 -0300

    set framerate to 28

--- SFMLGame/Effect.cpp
+++ SFMLGame/Effect.cpp
@@ -1,29 +1,30 @@
 #include "Effect.h"
 
 void MoveEffect::update(float elapsed_time) {
-	// TODO make it smoother under 60 FPS.
-	if ((time_count += elapsed_time) >= seconds_per_pixel) {
+	//if ((time_count += elapsed_time) >= seconds_per_pixel)
+	float pixels = 4.f;
+	{
 		time_count = 0;
 		if (character != nullptr) {
 			switch (direction) {
 			case Direction::UP:
-				character->move(0.f, -1.f);
+				character->move(0.f, -pixels);
 				break;
 			case Direction::DOWN:
-				character->move(0.f, 1.f);
+				character->move(0.f, pixels);
 				break;
 			case Direction::LEFT:
-				character->move(-1.f, 0.f);
+				character->move(-pixels, 0.f);
 				character->face_left();
 				break;
 			case Direction::RIGHT:
-				character->move(1.f, 0.f);
+				character->move(pixels, 0.f);
 				character->face_right();
 				break;
 			}
 		}
 		on_update();
-		if (++moved_pixels == 16) {
+		if ((moved_pixels += pixels) == 16) {
 			stop_running();
 		}
 	}
--- config/settings.lua
+++ config/settings.lua
@@ -8,8 +8,8 @@ fullscreen = false
 size_modifier = 3
 
 -- use_vsync and limit_framerate should not be used together.
-use_vsync = true
+use_vsync = false
 limit_framerate = not use_vsync
 
 -- This option takes effect if limit_framerate is true. The game does not run well under 60 fps.
-set_framerate = 30
+set_framerate = 28
