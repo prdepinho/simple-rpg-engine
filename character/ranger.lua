96769e9bc71604719c14457cd8ca8661ec4f0d09
Paulo Pinho <prdepinho@gmail.com>
2020-06-01 21:30:03 -0300

    create some characters & script them

--- character/ranger.lua
+++ character/ranger.lua
@@ -1,15 +1,17 @@
 
 package.path = package.path .. ";../character/?.lua"
 local animations = require "animations"
+local common = require "common"
 
 
 animation = animations.types.ranger
 
 
-function on_turn()
+function on_turn(id)
   print("Ranger: on_turn()")
 end
 
-function on_idle()
+function on_idle(id)
   print("Ranger: on_idle()")
+  common.idle_walk(id)
 end
