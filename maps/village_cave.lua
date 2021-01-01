2650070b4ff5835e56e0c812f105f19bf9068ba7
Paulo Pinho <prdepinho@gmail.com>
2021-01-01 13:28:14 -0300

    revise controls and cave

---
+++ maps/village_cave.lua
@@ -0,0 +1,28 @@
+
+package.path = package.path .. ";../maps/?.lua"
+local Map = require "map"
+
+local VillageCave = Map:new()
+
+function VillageCave:new(o, control)
+  o = o or Map:new(o, control)
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function VillageCave:create()
+  Map.create(self)
+end
+
+function VillageCave:enter()
+  Map.enter(self)
+end
+
+function VillageCave:exit()
+  Map.exit(self)
+end
+
+return VillageCave
+
+
