5df59266d60b9a7997c33c68eaa84afa2d4444de
Paulo Pinho <prdepinho@gmail.com>
2021-02-21 12:03:28 -0300

    add medeas portrait

--- maps/royal_bedroom.lua
+++ maps/royal_bedroom.lua
@@ -22,6 +22,42 @@ function RoyalBedroom:exit()
   Map.exit(self)
 end
 
+function RoyalBedroom:portrait(event, x, y, character_name, object_name)
+  if character_name == 'player' and event == 'interact' then
+    local dialogue = {
+      start = {
+        text = "There is a portrait here.",
+        options = {
+          { text = "Leave it.", go_to = 'end' },
+          { text = "Look at it.", go_to = 'image' },
+        }
+      },
+      image = {
+        text = "You take a closer look.",
+        go_to = 'end',
+        callback = function()
+          local dialogue = {
+            start = {
+              foreground = {
+                image = "lady.png",
+                origin = { x = 0, y = -50 }
+                -- origin = { x = 0, y = 0 },
+                -- pan_speed = { x = 0, y = -0.2 },
+                -- total_duration = 5.0,
+                -- still_duration = 1.0
+              },
+              text = "This is princess Medea in traditional garbs.",
+              go_to = 'end'
+            }
+          }
+          sfml_illustrated_dialogue(dialogue)
+        end
+      }
+    }
+    sfml_dialogue(dialogue)
+  end
+end
+
 return RoyalBedroom
 
 
